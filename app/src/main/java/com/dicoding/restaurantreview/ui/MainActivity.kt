205eb8bbcc674a9c0aa7692deebcd2f2bbb2fc36
wanmuhtd <ryskrnzo@gmail.com>
2024-10-13 20:13:51 +0700

    Adding event wrapper

--- app/src/main/java/com/dicoding/restaurantreview/ui/MainActivity.kt
+++ app/src/main/java/com/dicoding/restaurantreview/ui/MainActivity.kt
@@ -20,6 +20,7 @@ import com.dicoding.restaurantreview.data.response.Restaurant
 import com.dicoding.restaurantreview.data.response.RestaurantResponse
 import com.dicoding.restaurantreview.data.retrofit.ApiConfig
 import com.dicoding.restaurantreview.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -52,6 +53,16 @@ class MainActivity : AppCompatActivity() {
         mainViewModel.isLoading.observe(this) {
             showLoading(it)
         }
+
+        mainViewModel.snackbarText.observe(this, {
+            it.getContentIfNotHandled()?.let { snackBarText ->
+                Snackbar.make(
+                    window.decorView.rootView,
+                    snackBarText,
+                    Snackbar.LENGTH_SHORT
+                ).show()
+            }
+        })
 
         binding.btnSend.setOnClickListener { view ->
             mainViewModel.postReview(binding.edReview.text.toString())
--- app/src/main/java/com/dicoding/restaurantreview/ui/MainViewModel.kt
+++ app/src/main/java/com/dicoding/restaurantreview/ui/MainViewModel.kt
@@ -12,6 +12,9 @@ import com.dicoding.restaurantreview.data.retrofit.ApiConfig
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import com.dicoding.restaurantreview.util.Event
+
+
 
 class MainViewModel : ViewModel() {
 
@@ -23,6 +26,9 @@ class MainViewModel : ViewModel() {
 
     private val _isLoading = MutableLiveData<Boolean>()
     val isLoading: LiveData<Boolean> = _isLoading
+
+    private val _snackbarText = MutableLiveData<Event<String>>()
+    val snackbarText: LiveData<Event<String>> = _snackbarText
 
     companion object{
         private const val TAG = "MainViewModel"
@@ -65,6 +71,7 @@ class MainViewModel : ViewModel() {
                 _isLoading.value = false
                 if (response.isSuccessful) {
                     _listReview.value = response.body()?.customerReviews
+                    _snackbarText.value = Event(response.body()?.message.toString())
                 } else {
                     Log.e(TAG, "onFailure: ${response.message()}")
                 }
